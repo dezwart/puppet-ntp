ee44f371b575fd61ea26f55cf1cb3c2bfafa4658
Pete de Zwart <dezwart@gmail.com>
2012-12-27 23:07:03 +1100

    Linted.

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,28 +9,30 @@
 #
 # == Examples
 #
-#    class { 'ntp':
-#        servers => [ 'ntp0.example.com', 'ntp1.example.com' ],
-#    }
+#  class { 'ntp':
+#    servers => [ 'ntp0.example.com', 'ntp1.example.com' ],
+#  }
 #
 class ntp( $servers = undef ) {
-    package { 'ntp':
-        ensure  => installed,
-    }
+  package { 'ntp':
+    ensure  => installed,
+  }
 
-    file { '/etc/ntp.conf':
-        ensure  => file,
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        content => template('ntp/ntp.conf.erb'),
-        require => Package['ntp'],
-    }
+  file { '/etc/ntp.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => template('ntp/ntp.conf.erb'),
+    require => Package['ntp'],
+  }
 
-    service { 'ntp':
-        ensure      => running,
-        enable      => true,
-        require     => Package['ntp'],
-        subscribe   => File['/etc/ntp.conf'],
-    }
+  service { 'ntp':
+    ensure    => running,
+    enable    => true,
+    require   => Package['ntp'],
+    subscribe => File['/etc/ntp.conf'],
+  }
 }
+
+/* vim: set ts=2 sw=2 sts=2 tw=0 et:*/
